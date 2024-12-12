33117ebd0713622659316f55b877540620bbc813
jramac <jramac7@gmail.com>
2024-12-12 17:57:42 +0100

    dodani custom shaderi

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,9 +16,13 @@ renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement);
 
 //create a sphere
+const texture = new THREE.TextureLoader().load('./img/globe.jpg')
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5,50,50),
-    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('./img/globe.jpg')})
+    new THREE.ShaderMaterial({
+        vertexShader,
+        fragmentShader
+    })
 )
 scene.add(sphere)
 
