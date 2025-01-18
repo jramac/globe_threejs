aa32a4bf1f6916343624669c8e4c0bc73efeba4f
jramac <jramac7@gmail.com>
2025-01-18 12:07:00 +0100

    7 segmentni cilindar

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ document.body.appendChild(renderer.domElement);
 
 //create a sphere
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5,50,50),
+    new THREE.CylinderGeometry(1,1,3,7),
     new THREE.ShaderMaterial({
         vertexShader,
         fragmentShader,
@@ -34,11 +34,13 @@ const sphere = new THREE.Mesh(
     })
 )
 scene.add(sphere)
+sphere.rotation.z = Math.PI/2;
+//sphere.position.y = 4;
 //sphere.position.y = 4;
 
 
 
-camera.position.z = 8;
+camera.position.z = 3;
 
 const composer =  new EffectComposer(renderer);
 const renderPass = new RenderPass(scene,camera);
